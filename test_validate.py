1b45a495b090725ca19ee61c68c7b7c9ea30c577
marissa <mrr24@Squiddy.localdomain>
2026-03-25 22:37:19 -0400

    sanitation finished

--- test_validate.py
+++ test_validate.py
@@ -8,11 +8,177 @@ class Test_TestValidate(unittest.TestCase):
 
     def test_zip_bad(self):
         #ABUSE
-        f = open("blns.payloads", "rb")
+        f = open("blns.txt", "rb")
 
         for line in f:
             print(f"Attempting {line}")
             self.assertFalse(Validate.zip(str(line)))
+
+    def test_minor_happy(self):
+        #HAPPY PATH
+        good_vals = [1,2,10,11,12]
+            
+        for val in good_vals:
+            print(f'testing {val}')
+            self.assertTrue(Validate.minor(val))
+
+    def test_minor_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+            
+        for line in f:
+            print(f'testing {line}')
+            if line == "0" and line == "1":
+                continue
+            self.assertFalse(Validate.minor(str(line)))
+
+    def test_email_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.email("email@example.com"))
+
+    def test_email_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.email(str(line)))
+
+    def test_lat_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.is_lat("45.0"))
+
+    def test_lat_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.is_lat(str(line)))
+
+    def test_long_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.is_long("100.0"))
+
+    def test_long_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.is_long(str(line)))
+
+    def test_domain_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.is_domain("example.com"))
+
+    def test_domain_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.is_domain(str(line)))
+
+    def test_url_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.is_url("http://example.com"))
+
+    def test_url_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.is_url(str(line)))
+
+    def test_grade_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.grade(85))
+
+    def test_grade_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.grade(str(line)))
+
+    def test_good_sql(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.sanitize("SELECT * FROM users"))
+
+    def test_bad_sql(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.sanitize(str(line)))
+    
+    def test_no_sql_injection_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.no_sql_injection("hello world"))
+    
+    def test_no_sql_injection_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.no_sql_injection(str(line)))
+
+    def test_strip_null(self):
+        #HAPPY PATH
+        test = ["hello\x00world", None, "hello\x00None\x00world"]
+
+        sanitized = [Validate.strip_null(item) for item in test]
+
+        print(sanitized[0], sanitized[1], sanitized[2])
+
+    def test_strip_null_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            sanitized = Validate.strip_null(line)
+            self.assertNotIn(b"None", sanitized)
+            self.assertNotIn(b"\x00", sanitized)
+    def test_ip_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.ip("168.192.1.3"))
+
+    def test_ip_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.ip(str(line)))
+    def test_mac_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.mac(str("AA:BB:CC:DD:EE:FF")))
+
+    def test_mac_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.mac(str(line)))
+
+    def test_md5_happy(self):
+        #HAPPY PATH
+        self.assertTrue(Validate.md5("d41d8cd98f00b204e9800998ecf8427e"))
+
+    def test_md5_bad(self):
+        #ABUSE
+        f = open("blns.txt", "rb")
+
+        for line in f:
+            print(f"Attempting {line}")
+            self.assertFalse(Validate.md5(str(line)))
 
 if __name__ == '__main__':
     unittest.main()
--- validate.py
+++ validate.py
@@ -10,23 +10,38 @@ class Validate:
   
   @staticmethod
   def minor(age):
-    if age > 17:
+    try:
+      age = int(age)
+      if age > 17:
+        return False
+      return True
+    except Exception as e:
+      #print(e)
       return False
-    return True
   
   @staticmethod 
   def email(input):
-    if "@" in input and "." in input:
+    if re.match("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", input):
       return True
     return False
   
   @staticmethod
-  def is_lat(number):
-    return number >= -90 and number <= 90
+  def is_lat(lat):
+    try:
+      lat = float(lat)
+      return lat >= -90 and lat <= 90
+    except (ValueError, TypeError) as e:
+      #print(e)
+      return False
   
   @staticmethod
-  def is_lng(number):
-    return number >= -180 and number <= 180
+  def is_long(long):
+    try:
+      long = float(long)
+      return long >= -180 and long <= 180
+    except (ValueError, TypeError) as e:
+      #print(e)
+      return False
   
   @staticmethod
   def is_domain(input):
@@ -36,60 +51,93 @@ class Validate:
   
   @staticmethod
   def is_url(input):
-    if re.match("^http(s)?:\\/\\/(.*?)\\.[a-z]{2,52}\\/.*$", input):
-      return True
-    return False
-  
+      # regex obtained from https://uibakery.io/regex-library/url-regex-python
+      if re.match("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$", input):
+        return True
+      return False
+
   @staticmethod
   def grade(value):
-    if value < 60:
-      return 'F'
-    elif value < 70:
-      return 'D'
-    elif value < 80:
-      return 'C'
-    elif value < 90:
-      return 'B'
+
+    value = str(value).strip()
+    #regex checks if this value is a number with two decimal places at most
+    if not re.fullmatch("^\d+(\.\d{1,2})?", value):
+        return False
+    #convert the value back to a float
+    value = float(value)
+
+    if value < 0 or value > 100:
+      raise ValueError("Value must be between 0 and 100")
+  
+    if value <= 59.99:
+      grade = 'F'
+      print(grade)
+      return grade
+    elif value <= 69.99:
+      grade = 'D'
+      print(grade)
+      return grade
+    elif value <= 79.99:
+      grade = 'C'
+      print(grade)
+      return grade
+    elif value <= 89.99:
+      grade = 'B'
+      print(grade)
+      return grade
     
-    return 'A'
+    grade = 'A'
+    print(grade)
+    return grade
   
   '''
   Typing added to enhance validation
   '''
   @staticmethod
-  def sanitize(sql : str) -> str:
-    sql = sql.upper().replace("ADMIN", "")
-    sql = sql.upper().replace("OR", "")
-    sql = sql.upper().replace("COLLATE", "")
-    sql = sql.upper().replace("DROP", "")
-    sql = sql.upper().replace("AND", "")
-    sql = sql.upper().replace("UNION", "")
-    sql = sql.replace("/*", "")
-    sql = sql.replace("*/", "")
-    sql = sql.replace("//", "")
-    sql = sql.replace(";", "")
-    sql = sql.replace("||", "")
-    sql = sql.replace("&&", "")
-    sql = sql.replace("--", "")
-    sql = sql.replace("#", "")
-    sql = sql.replace("=", "")
-    sql = sql.replace("!=", "")
-    sql = sql.replace("<>", "")
+  def sanitize(sql):
+    #read only SQL
 
-    return sql
+    sql = sql.strip()
 
+    # if sql does not start with a select statement then return false
+    if not sql.upper().startswith("SELECT"):
+      return False
+    # if sql contains any of the following characters then return false
+    if re.search("--|#|/\*", sql):
+      return False
+    return True
+  
+  def no_sql_injection(input):
+    # check if input contains any of the following characters
+    if not re.match(r"^[a-zA-Z0-9 ]+$", input):
+      return False
+    if re.search("SELECT|INSERT|UPDATE|DELETE|DROP|ALTER|CREATE", input, re.IGNORECASE):
+      return False
+    return True
+  
+    
   @staticmethod
   def strip_null(input : str) -> str:
-    input = input.replace("None", "")
-
-    return input
-
+    if input == None:
+      return ""
+    if isinstance(input, bytes):
+          # remove b"None" and null bytes
+        sanitized = input.replace(b"None", b"").replace(b"\x00", b"")
+    elif isinstance(input, str):
+          # remove "None" and null bytes
+        sanitized = input.replace("None", "").replace("\x00", "")
+    else:
+          # anything else, just return as-is
+        sanitized = input
+    return sanitized
   '''
   TODO: Implement IP validation. Consider white-list, regex.
   '''
   @staticmethod
-  def ip(input) -> bool:
-    return True
+  def ip(input : int) -> int:
+    if re.match("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$", input):
+      return True
+    return False
   
   '''
   TODO: Implement MAC address validation. Consider white-list, regex.
@@ -98,11 +146,15 @@ class Validate:
   '''
   @staticmethod
   def mac(input) -> bool:
-    return True
-  
+    if re.match("^([0-9A-Fa-f]{2}[-]?[\:s]?){5}([0-9A-Fa-f]{2})$", input):
+      return True
+    return False
+
   '''
   TODO: Implement md5 validation. Consider white-list, regex.
   '''
   @staticmethod
   def md5(input) -> bool:
-    return True
+    if re.match("^[a-fA-F0-9]{32}$", input):
+      return True
+    return False
